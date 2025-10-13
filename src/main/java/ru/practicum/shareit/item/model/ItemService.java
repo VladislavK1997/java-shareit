a43783aecf43b7858cf6c9de267a7870a9298f5b
VladK <vlad.katrich1997@yandex.ru>
2025-10-13 11:54:21 +0300

    .

--- src/main/java/ru/practicum/shareit/item/model/ItemService.java
+++ src/main/java/ru/practicum/shareit/item/model/ItemService.java
@@ -6,8 +6,12 @@ import java.util.List;
 
 public interface ItemService {
     ItemDto addItem(ItemDto itemDto, Long ownerId);
+
     ItemDto updateItem(Long itemId, ItemDto itemDto, Long ownerId);
+
     ItemDto getItemById(Long itemId);
+
     List<ItemDto> getAllItemsByOwner(Long ownerId);
+
     List<ItemDto> searchItems(String text);
 }
